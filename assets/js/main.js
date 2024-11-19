9d903b96fd4be65460b8d5e7eb03227593529fc4
saman khadivar <91559399+Samdopika@users.noreply.github.com>
2024-11-18 19:27:23 -0800

    s

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,9 +15,9 @@
 
     // === logo change
     if (ud_header.classList.contains("sticky")) {
-      logo.src = "assets/images/logo/CyruTechDark.png";
+      logo.src = "../assets/images/logo/CyruTechDark.png";
     } else {
-      logo.src = "assets/images/logo/CyruTech-logo.png";
+      logo.src = "../assets/images/logo/CyruTech-logo.png";
     }
 
     // show or hide the back-top-top button
